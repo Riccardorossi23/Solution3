3452fb0e54938ae837f92a8630351b53f8d693f0
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:18:10 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Totale spesa:");
+            double spesa = double.Parse(Console.ReadLine());
+            if (spesa > 100)
+            {
+                double sconto = spesa * 20 / 100;
+                Console.WriteLine($"il totale è {sconto}");
+            }
+            else 
+            {
+                Console.WriteLine($"lo sconto non puo essere effettuato");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Totale spesa:");
+            double spesa = double.Parse(Console.ReadLine());
+            if (spesa > 100)
+            {
+                double sconto = spesa * 20 / 100;
+                double totale = spesa - sconto;
+                Console.WriteLine($"il totale è {totale}");
+            }
+            else
+            {
+                Console.WriteLine($"lo sconto non puo essere effettuato");
+            }
+            Console.ReadLine();
+        }
+    }
+}
